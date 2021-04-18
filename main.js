63fe1cc8872af15a6381a85491916d5169fa78c9
Viraj-Vijay-Hire <78733888+Viraj-Vijay-Hire@users.noreply.github.com>
2021-04-18 13:57:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas=getContext("2d");
+
+car1_width = 120;
+car1_height = 70;
+car1_image = "https://postimg.cc/9rqYz9HM";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image = "th.jfif.jpg";
+car2_x = 10;
+car2_y = 100;
+
+background_image = "https://postimg.cc/p9D2dqqZ";
